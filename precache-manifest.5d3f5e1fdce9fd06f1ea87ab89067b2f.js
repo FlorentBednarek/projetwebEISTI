ae5597b9f6c58c5a8ab9b5d3d4d9a6bc9f93611a
Florent Bednarek <florent.bednarek@eisti.fr>
2020-06-01 00:36:02 +0200

    Updates

---
+++ precache-manifest.5d3f5e1fdce9fd06f1ea87ab89067b2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b4e5603573b7aa9282dec750c6f408",
+    "url": "/projetwebEISTI/index.html"
+  },
+  {
+    "revision": "0f13036e44baac0e4cb9",
+    "url": "/projetwebEISTI/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "7e7a449787669764e87c",
+    "url": "/projetwebEISTI/static/css/main.b524c0da.chunk.css"
+  },
+  {
+    "revision": "0f13036e44baac0e4cb9",
+    "url": "/projetwebEISTI/static/js/2.71993ef5.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/projetwebEISTI/static/js/2.71993ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7a449787669764e87c",
+    "url": "/projetwebEISTI/static/js/main.629d67c8.chunk.js"
+  },
+  {
+    "revision": "b26cae2f639745fb27e4",
+    "url": "/projetwebEISTI/static/js/runtime-main.cd6aba70.js"
+  },
+  {
+    "revision": "1e0b9a138baceb2ef52c187659d6ce0d",
+    "url": "/projetwebEISTI/static/media/logo.1e0b9a13.png"
+  }
+]);
